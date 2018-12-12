7cbf39f4b414de3462fde3e9bad4c4a92ceaa94f
Antonio Angosto <antonio.angosto@goumh.umh.es>
2018-12-12 13:37:09 +0100

    Added documentation of the app pipes

--- src/app/pipes/sanitize-html/sanitize-html.pipe.ts
+++ src/app/pipes/sanitize-html/sanitize-html.pipe.ts
@@ -5,11 +5,17 @@ import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
   name: 'sanitizeHtml'
 })
 export class SanitizeHtmlPipe implements PipeTransform {
+
   constructor(private sanitizer: DomSanitizer) {
   }
 
+  /**
+   * Makes the view trust the injected HTML tags
+   *
+   * @param {string} value Block of unsanitized HTML tags
+   * @returns {SafeHtml} The sanitized HTML tags
+   */
   transform(value: string): SafeHtml {
     return this.sanitizer.bypassSecurityTrustHtml(value);
   }
-
 }
