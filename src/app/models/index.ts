7832a4d7a98f06f9aef9e2ed7dec1726d7b68420
Alberto Moreno <amoreno@goumh.umh.es>
2019-05-08 13:53:51 +0200

    First roles guard configuration

---
+++ src/app/models/index.ts
@@ -0,0 +1,6 @@
+export * from './ecoe';
+export * from './organization';
+export * from './planner';
+export * from './role';
+export * from './schedule';
+export * from './user';
--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {AdminComponent} from './admin.component';
 import {HomeAdminComponent} from './home-admin/home-admin.component';
 import {AuthenticationGuard} from '../../guards/authentication/authentication.guard';
+import {Role} from '../../models';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
     canActivate: [AuthenticationGuard],
+    data: {roles: [Role.Admin]},
     children: [
       {
         path: '',
@@ -26,4 +28,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {
+}
