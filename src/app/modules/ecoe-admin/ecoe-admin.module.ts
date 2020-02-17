1768cf64ca2ff1b8e735b882edd1a5548c5a8336
mmartinezf1 <m.martinezf@goumh.umh.es>
2020-02-17 13:15:37 +0100

    New app modules

---
+++ src/app/modules/ecoe-admin/ecoe-admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EcoeAdminRoutingModule } from './ecoe-admin-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EcoeAdminRoutingModule
+  ]
+})
+export class EcoeAdminModule { }
---
+++ src/app/modules/ecoe/ecoe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EcoeRoutingModule } from './ecoe-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EcoeRoutingModule
+  ]
+})
+export class EcoeModule { }
