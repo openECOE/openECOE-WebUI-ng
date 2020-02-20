9b01e088ef513df4f96fdc39d987a861beed971e
yaroslav trush <ytrush@goumh.umh.es>
2020-02-20 09:47:41 +0100

    fixed order number on add new question and removed unused code.

--- src/app/models/ecoe.ts
+++ src/app/models/ecoe.ts
@@ -76,7 +76,7 @@ export class Question extends Item {
   questionType: string;
   order: number;
 
-  addOption ? = Route.POST<Option>('/option');
+  addOption ? = Route.POST<Option>('/options');
 
   area: Area;
 
@@ -92,7 +92,7 @@ export interface RowQuestion {
   reference: any[] | string;
   area: any[] | Area;
   questionType: any[] | string;
-  optionsNumber?: number | number;
+  optionsNumber?: number;
   points?: any[];
   options?: Option[];
   qblocks?: number[];
