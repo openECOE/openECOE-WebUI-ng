02e05295c0ebb1bf501cf7c6e1c2710200090199
mmartinezf1 <m.martinezf@goumh.umh.es>
2020-02-21 15:29:14 +0100

    Developing...

--- src/app/modules/ecoe-admin/ecoe-admin-routing.module.ts
+++ src/app/modules/ecoe-admin/ecoe-admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AreasComponent } from './areas/areas.component';
+import { StationsComponent } from './stations/stations.component';
 
 const routes: Routes = [
-  { path: 'areas', component: AreasComponent }
+  { path: 'areas', component: AreasComponent },
+  { path: 'stations', component: StationsComponent }
 ];
 
 @NgModule({
