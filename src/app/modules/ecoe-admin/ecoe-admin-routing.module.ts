60f7a45654da028dfc9f8a22f01d1895d621e647
mmartinezf1 <m.martinezf@goumh.umh.es>
2020-02-24 10:25:42 +0100

    Review EcoeAdminRoutingModule

--- src/app/modules/ecoe-admin/ecoe-admin-routing.module.ts
+++ src/app/modules/ecoe-admin/ecoe-admin-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { AreasComponent } from './areas/areas.component';
 import { StationsComponent } from './stations/stations.component';
 import {EcoeInfoComponent} from '../ecoe/ecoe-info/ecoe-info.component';
-import { QuestionsListComponent } from 'src/app/components/questions-list/questions-list.component';
 import { StationDetailsComponent } from './station-details/station-details.component';
 
 const routes: Routes = [
