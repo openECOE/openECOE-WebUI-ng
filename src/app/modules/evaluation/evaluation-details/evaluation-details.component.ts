2db955293fa3407513f77b6e5268ab39cbf5ba74
yaroslav trush <ytrush@goumh.umh.es>
2020-02-25 12:34:39 +0100

    added shifts filter in evaluation-details

--- src/app/modules/evaluation/evaluation-details/evaluation-details.component.ts
+++ src/app/modules/evaluation/evaluation-details/evaluation-details.component.ts
@@ -28,6 +28,12 @@ export class EvaluationDetailsComponent implements OnInit {
   momentRef = moment;
   refresh: boolean = false;
   isSpinning: boolean = false;
+  currentStep: number = 0;
+
+  selectedEcoeDay;
+  selectedRound: Round;
+  selectedStation: Station;
+  showStepsForm: boolean = true;
 
   constructor(private route: ActivatedRoute,
               private location: Location,
@@ -40,8 +46,8 @@ export class EvaluationDetailsComponent implements OnInit {
   async ngOnInit() {
     if (this.authService.userLogged) {
       this.momentRef.locale(this.shared.getUsersLocale('en-US'));
-      this.ecoeId = +this.route.snapshot.params.ecoeId;
-      this.ecoe = (await ECOE.fetch(this.ecoeId)) as ECOE;
+      await this.getUrlParams();
+      this.initComboFilter();
       this.getData(this.ecoe).then(() => {
           this.getShiftDays(this.shifts);
           this.getSelectedShift();
@@ -50,6 +56,39 @@ export class EvaluationDetailsComponent implements OnInit {
       });
     } else {
       this.authService.logout();
+    }
+  }
+
+  getUrlParams() {
+    const params = this.route.snapshot.params;
+
+    this.ecoeId = +params['ecoeId'];
+    this.selectedEcoeDay = params['date'];
+
+    return  new Promise((resolve, reject) => {
+      const ecoePromise = ECOE.fetch(this.ecoeId)
+        .then((ecoe: ECOE) => this.ecoe = ecoe )
+        .catch((err) => reject(err));
+
+      if (params['roundId'] && params['stationId']) {
+        const roundPromise = Round.fetch(params['roundId'])
+          .then((round: Round) => this.selectedRound = round )
+          .catch((err) => reject(err));
+
+        const stationPromise = Station.fetch(params['stationId'])
+          .then((station: Station) => this.selectedStation = station )
+          .catch((err) => reject(err));
+
+        Promise.all([roundPromise, stationPromise, ecoePromise]).then(() => resolve());
+      } else {
+        Promise.all([ecoePromise]).then(() => resolve());
+      }
+    });
+  }
+
+  initComboFilter() {
+    if (this.selectedEcoeDay && this.selectedRound && this.selectedStation) {
+      this.showStepsForm = false;
     }
   }
 
@@ -62,6 +101,7 @@ export class EvaluationDetailsComponent implements OnInit {
       }
     });
     this.ecoeDays = aux_arr;
+    this.selectedEcoeDay = this.ecoeDays[0];
   }
 
   onChangeECOEDay(shiftDate: string) {
@@ -73,7 +113,7 @@ export class EvaluationDetailsComponent implements OnInit {
         return x;
       }
     });
-    this.setSelectedShift(shiftDate);
+    // this.setSelectedShift(shiftDate);
   }
 
   onChangeRound(round: Round) {
@@ -119,6 +159,9 @@ export class EvaluationDetailsComponent implements OnInit {
     const roundsPromise = ecoe.rounds()
       .then((rounds: Round[]) => {
         this.rounds = rounds;
+        if (!this.selectedRound && rounds.length > 0) {
+          this.selectedRound = rounds[0];
+        }
       });
     const shiftsPromise = ecoe.shifts()
       .then((shifts: any[]) => {
@@ -136,11 +179,33 @@ export class EvaluationDetailsComponent implements OnInit {
     return Station.query({
       where: {ecoe: this.ecoeId},
       sort: {order: false}
-    }).then( (stations: Station[]) =>
-      this.stations = stations);
+    }).then( (stations: Station[]) => {
+      this.stations = stations;
+      if (!this.selectedStation) {
+        this.selectedStation = this.stations[0];
+      }
+    });
   }
 
   onBack() {
     this.router.navigate(['/ecoe']);
+  }
+
+  onStepChange($event: any) {
+    if ($event === 'NEXT') {
+      this.currentStep++;
+    } else if ($event === 'BACK') {
+      this.currentStep--;
+    }
+  }
+
+  finish() {
+    this.onChangeECOEDay(this.selectedEcoeDay);
+    this.setVisibleSteps(false);
+  }
+
+  setVisibleSteps(val: boolean) {
+    this.showStepsForm = val;
+    this.currentStep = 0;
   }
 }
--- src/app/modules/evaluation/evaluation-routing.module.ts
+++ src/app/modules/evaluation/evaluation-routing.module.ts
@@ -6,6 +6,8 @@ import {EvaluateComponent} from './evaluate/evaluate.component';
 
 const routes: Routes = [
   { path: '', component: EvaluationDetailsComponent, pathMatch: 'full'},
+  { path: 'date/:date/round/:roundId/station/:stationId', component: EvaluationDetailsComponent},
+  { path: 'station/:stationId/shift/:shiftId/round/:roundId/date/:date', component: EvaluateComponent},
   { path: 'station/:stationId/shift/:shiftId/round/:roundId', component: EvaluateComponent},
   { path: '**', redirectTo: '/'}
 ];
