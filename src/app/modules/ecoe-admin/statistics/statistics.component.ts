89797b4c44ab9fd7c42d926aa8df75d640944cb8
Pasqual Martínez Botella <pasqual.martinezb@umh.es>
2022-09-27 11:44:57 +0200

    Inclusion de breadcrumb y vuelta atrás en caja Resultados

--- src/app/modules/ecoe-admin/statistics/statistics.component.ts
+++ src/app/modules/ecoe-admin/statistics/statistics.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute, Router} from '@angular/router';
+import {Area, EditCache, RowArea, ECOE} from '../../../models';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.less']
 })
 export class StatisticsComponent implements OnInit {
-
-  constructor() { }
+  editCache:    EditCache[] = [];
+  ecoeId:       number;
+  ecoe:         ECOE;
+  ecoe_name:    String;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.ecoeId = +params.ecoeId;
+
+      ECOE.fetch<ECOE>(this.ecoeId, {cache: false}).then(value => {
+        this.ecoe = value;
+        this.ecoe_name = this.ecoe.name;
+  
+        const excludeItems = [];
+    });
+    
+      
+    });
   }
 
+  
+
+  onBack() {
+    this.router.navigate(['/ecoe/' + this.ecoeId + '/admin']).finally();
+  }
 }
