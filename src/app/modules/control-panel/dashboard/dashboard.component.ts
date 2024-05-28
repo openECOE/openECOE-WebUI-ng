b59e34e36c52b1f369350f1fe108050507e9cbc5
elCrimar <cristobalmartimendez@gmail.com>
2024-05-28 13:57:39 +0200

    Arreglado bug que mostraba undefined al recargar la pagina

--- src/app/modules/control-panel/dashboard/dashboard.component.ts
+++ src/app/modules/control-panel/dashboard/dashboard.component.ts
@@ -1,47 +1,74 @@
-import {Component, OnInit} from '@angular/core';
-import {Organization, User, UserLogged} from '@app/models';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Organization, User, UserLogged } from '@app/models';
 import { UserService } from '@app/services/user/user.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  userData: UserLogged;
-
+  userData: UserLogged | null = null;
   usersPage: any[] = [];
   organizationsPage: any[] = [];
-
   loading: boolean = false;
+  initialLoading: boolean = true;
+
+  private userDataSubscription: Subscription;
 
   constructor(
     private userService: UserService,
     private router: Router
-  ) {
-  }
+  ) { }
 
   ngOnInit() {
     this.userData = this.userService.userData;
-    this.loadUsers();
-    this.loadOrganizations();
+
+    this.userDataSubscription = this.userService.userDataChange.subscribe(userData => {
+      this.userData = userData;
+      if (userData) {
+        this.loadUsers();
+        this.loadOrganizations();
+      }
+      this.initialLoading = false;
+    });
+
+    if (this.userData) {
+      this.loadUsers();
+      this.loadOrganizations();
+      this.initialLoading = false;
+    } else {
+      this.userService.loadUserData().then(userData => {
+        this.userService.userData = userData;
+      });
+    }
+
     this.router.navigate(['control-panel/users']);
   }
 
+  ngOnDestroy() {
+    if (this.userDataSubscription) {
+      this.userDataSubscription.unsubscribe();
+    }
+  }
+
   loadUsers() {
-    this.loading = true;
-    User.query({
-      where: {organization: this.userData.user.organization}
-    }, {paginate: true})
-      .then((page) => this.usersPage = page)
-      .finally(() => this.loading = false);
+    if (this.userData) {
+      this.loading = true;
+      User.query({
+        where: { organization: this.userData.user.organization }
+      }, { paginate: true })
+        .then((page) => this.usersPage = page)
+        .finally(() => this.loading = false);
+    }
   }
 
   loadOrganizations() {
     this.loading = true;
-    Organization.query({}, {paginate: true})
+    Organization.query({}, { paginate: true })
       .then((page) => this.organizationsPage = page)
       .finally(() => this.loading = false);
   }
@@ -49,5 +76,4 @@ export class DashboardComponent implements OnInit {
   goBack() {
     this.router.navigate(['/ecoe']);
   }
-  
 }
