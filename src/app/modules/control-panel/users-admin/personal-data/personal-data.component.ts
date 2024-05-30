85aab08004a3b58435ab020e104ef3a0106fdc8d
elCrimar <cristobalmartimendez@gmail.com>
2024-05-30 12:01:10 +0200

    Agregado formulario de cambio de contraseña

--- src/app/modules/control-panel/users-admin/personal-data/personal-data.component.ts
+++ src/app/modules/control-panel/users-admin/personal-data/personal-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserLogged } from '@app/models';
+import { SharedService } from '@app/services/shared/shared.service';
 import { UserService } from '@app/services/user/user.service';
 
 @Component({
@@ -11,13 +12,25 @@ import { UserService } from '@app/services/user/user.service';
 export class PersonalDataComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    public shared: SharedService,
+    private fb: FormBuilder
   ) { }
 
-  userData: UserLogged; 
+  userData: UserLogged;
+  newPassword: string;
+  validateForm: FormGroup;
+  showEditPassword: boolean = false;
+  confirmDeleteText: string = '';
 
   ngOnInit(): void {
+    this.getPasswordForm();
+
     this.getUserData();
+    
+    this.validateForm.get('newPassword').valueChanges.subscribe(value => {
+      this.confirmDeleteText = value;
+    });
   }
 
   getUserData(): void {
@@ -27,7 +40,51 @@ export class PersonalDataComponent implements OnInit {
     console.log(this.userData);
   }
 
-  editUser() {
+  async getPasswordForm() {
+    this.validateForm = this.fb.group({
+      newPassword: [null,[Validators.required]],
+    });
   }
 
+  showModalEdit() {
+    this.showEditPassword = true;
+  }
+
+  closeModal() {
+    this.shared.cleanForm(this.validateForm);
+    this.showEditPassword = false;
+  }
+
+  async submitFormOrganization(form: FormGroup) {
+    this.shared.doFormDirty(form);
+    if (form.pending) {
+      const sub = form.statusChanges.subscribe(() => {
+        if (form.valid) {
+          this.submitForm(form.value);
+        }
+        sub.unsubscribe();
+      });
+    } else if (form.valid) {
+      this.submitForm(form.value);
+    }
+  }
+
+  async submitForm(value: any) {
+    try {
+      if (this.showEditPassword) {
+        await this.updatePassword(this.userData, value);
+      }
+      //this.getUserData();
+    } catch (error) {
+      console.error(error);
+    }
+    this.closeModal();
+  }
+
+  async updatePassword(userData: UserLogged, value: any) {
+    const updateData = {
+      password: value.newPassword,
+    };
+    await userData.user.update(updateData);
+  }
 }
