992822cb9cfdd9c639eccdfab6aafbd1bd32000c
elCrimar <cristobalmartimendez@gmail.com>
2024-06-03 10:24:37 +0200

    Ahora los guards se suscriben al userDataChange para comprobar si es Admin o Super a través de URL

--- src/app/guards/super-admin.guard.ts
+++ src/app/guards/super-admin.guard.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable, from } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { UserService } from '../services/user/user.service';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +14,26 @@ export class SuperAdminGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> {
-    return from(this.userService.ensureUserDataReady()).pipe(
-      map(() => {
-        const user = this.userService.userData;
-        if (user && user.isSuper) {
-          return true;
+    return new Observable<boolean | UrlTree>(observer => {
+      const user = this.userService.userData;
+      if (user) {
+        if (user.isSuper) {
+          observer.next(true);
         } else {
-          return this.router.createUrlTree(['/ecoe']);
+          observer.next(this.router.createUrlTree(['/ecoe']));
         }
-      })
-    );
+        observer.complete();
+      } else {
+        const subscription = this.userService.userDataChange.subscribe(userData => {
+          if (userData && userData.isSuper) {
+            observer.next(true);
+          } else {
+            observer.next(this.router.createUrlTree(['/ecoe']));
+          }
+          observer.complete();
+          subscription.unsubscribe();
+        });
+      }
+    });
   }
 }
