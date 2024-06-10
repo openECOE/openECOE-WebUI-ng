bcaab7fe57a3c567d0dad9bdd57951ddd12c4dd7
elCrimar <cristobalmartimendez@gmail.com>
2024-06-10 12:47:43 +0200

    Cambiado botones de la plantilla

--- src/app/modules/ecoe-results/generate-reports/generate-reports.component.ts
+++ src/app/modules/ecoe-results/generate-reports/generate-reports.component.ts
@@ -13,7 +13,6 @@ import * as docx from "docx";
 import { saveAs } from "file-saver";
 import { ECOE, Template } from "@app/models";
 import { from } from "rxjs";
-import { dirname } from "path";
 import { TemplateService } from "@app/services/report-template/template.service";
 
 @Component({
@@ -43,7 +42,7 @@ export class GenerateReportsComponent implements OnInit {
   config: any = {
     minHeight: 600,
     maxWidth: 900,
-    buttons: "undo,redo,|,font,fontsize,|,bold,italic,underline,eraser,|,superscript,subscript,|,indent,outdent,left,center,right,justify,|,ul,table,selectall,hr,|,link,image,print,|,varsList,source,preview,fullsize",
+    buttons: "undo,redo,|,font,fontsize,|,bold,italic,underline,eraser,|,superscript,subscript,|,indent,outdent,left,center,right,justify,|,ul,table,selectall,hr,|,link,image,print,|,downloadDoc, uploadDoc,|,varsList,preview,fullsize,source",
     uploader: {
       insertImageAsBase64URI: true
     },
@@ -86,10 +85,32 @@ export class GenerateReportsComponent implements OnInit {
           } else {
               console.error(`No se encontró ninguna clave para el texto seleccionado: ${selectedText}`);
           }
-      },
+        },
         tooltip: 'Insertar etiqueta'
-    }
-    
+      },
+      downloadDoc: {
+        icon: `<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 489.104 489.104" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M411.55,100.9l-94.7-94.7c-4.2-4.2-9.4-6.2-14.6-6.2H92.15c-11.4,0-20.8,9.4-20.8,20.8v330.8c0,11.4,9.4,20.8,20.8,20.8 h132.1V421l-16.6-15.2c-8.3-7.3-21.8-7.3-29.1,1s-7.3,21.8,1,29.1l52,47.9c3.1,3.1,14.6,10.2,29.1,0l52-47.9 c8.3-8.3,8.3-20.8,1-29.1c-8.3-8.3-20.8-8.3-29.1-1l-18.7,17.2v-50.5h132.1c11.4,0,19.8-9.4,19.8-19.8V115.5 C417.85,110.3,415.75,105.1,411.55,100.9z M324.15,70.4l39.3,38.9h-39.3V70.4z M265.95,331.9v-130c0-11.4-9.4-20.8-20.8-20.8 c-11.4,0-20.8,9.4-20.8,20.8v130h-111.3V41.6h169.6v86.3c0,11.4,9.4,20.8,20.8,20.8h74.9v183.1h-112.4V331.9z"></path> </g> </g></svg>`,
+        exec: (editor, current, control) => {
+          this.downloadAsDocx();
+        },
+        tooltip: 'Descargar docx'
+      },
+      uploadDoc: {
+        icon: '<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 488.9 488.9" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M411.448,100.9l-94.7-94.7c-4.2-4.2-9.4-6.2-14.6-6.2h-210.1c-11.4,0-20.8,9.4-20.8,20.8v330.8c0,11.4,9.4,20.8,20.8,20.8 h132.1v95.7c0,11.4,9.4,20.8,20.8,20.8s20.8-9.4,20.8-19.8v-96.6h132.1c11.4,0,19.8-9.4,19.8-19.8V115.5 C417.748,110.3,415.648,105.1,411.448,100.9z M324.048,70.4l39.3,38.9h-39.3V70.4z M378.148,331.9h-112.3v-82.8l17.7,16.3 c10,10,25,3.1,28.1-1c7.3-8.3,7.3-21.8-1-29.1l-52-47.9c-8.3-7.3-20.8-7.3-28.1,0l-52,47.9c-8.3,8.3-8.3,20.8-1,29.1 c8.3,8.3,20.8,8.3,29.1,1l17.7-16.3v82.8h-111.4V41.6h169.6v86.3c0,11.4,9.4,20.8,20.8,20.8h74.9v183.2H378.148z"></path> </g> </g></svg>',
+        exec: (editor, current, control) => {
+          const input = document.createElement('input');
+          input.type = 'file';
+          input.accept = '.docx';
+          input.onchange = (event) => {
+            const file = (event.target as HTMLInputElement).files[0];
+            if (file) {
+              this.convertDocxToHtml(file);
+            }
+          };
+          input.click();
+        },
+        tooltip: 'Subir docx'
+      }
     }
   };
   
@@ -141,13 +162,6 @@ export class GenerateReportsComponent implements OnInit {
             }
         });
     });
-  }
-
-  onFileSelected(event: NzUploadChangeParam) {
-    const file = event.file.originFileObj as File;
-    if (file) {
-      this.convertDocxToHtml(file);
-    }
   }
 
   convertDocxToHtml(file: File) {
@@ -344,7 +358,6 @@ export class GenerateReportsComponent implements OnInit {
   public saveTemplate(): void {
     const htmlString = this.editorContent;
     from(this.template.createTemplate(this.ecoe, htmlString)).subscribe((template) => {
-      console.log("Plantilla guardada exitosamente:", template);
       this.router.navigate(["/ecoe/" + this.ecoeId + "/results"]);
     });
   }
