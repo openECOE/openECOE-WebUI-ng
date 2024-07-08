b83b14c5ef3a27df74a46b3d8dea1f24a32a492b
miguelaf0 <miapafe@gmail.com>
2024-07-08 10:58:10 +0200

    Fix problem that the user was not loading correctly

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,39 +1,33 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscriber } from 'rxjs';
 import { UserService } from '../services/user/user.service';
+import { UserLogged } from '@app/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(private router: Router, private userService: UserService) { }
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     return new Observable<boolean | UrlTree>(observer => {
-      const user = this.userService.userData;
-      if (user) {
-        if (user.isAdmin || user.isSuper) {
-          observer.next(true);
-        } else {
-          observer.next(this.router.createUrlTree(['/ecoe']));
-        }
-        observer.complete();
-      } else {
-        const subscription = this.userService.userDataChange.subscribe(userData => {
-          if (userData && userData.isAdmin || userData.isSuper) {
-            observer.next(true);
-          } else {
-            observer.next(this.router.createUrlTree(['/ecoe']));
-          }
-          observer.complete();
-          subscription.unsubscribe();
+      if(this.userService.userData) {
+        this.checkRole(this.userService.userData, observer);
+      }
+      else {
+        this.userService.userDataChange.subscribe((user: UserLogged) => {
+          this.checkRole(user, observer)
         });
       }
     });
+  }
+
+  private checkRole(user: UserLogged, observer: Subscriber<boolean | UrlTree>): void {
+    user.isAdmin 
+    ? observer.next(true)
+    : observer.next(this.router.createUrlTree(['/ecoe']));
+    observer.complete();
   }
 }
