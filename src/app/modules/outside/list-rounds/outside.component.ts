8e2d80ab26db0fe1f46585f3024839a869b2bd5f
elCrimar <cristobalmartimendez@gmail.com>
2024-07-11 13:25:07 +0200

    Implementacion temprana de las listas de organizaciones

--- src/app/modules/outside/list-rounds/outside.component.ts
+++ src/app/modules/outside/list-rounds/outside.component.ts
@@ -3,7 +3,14 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ECOEConfig, InfoData} from '../../../models/chrono';
 import {Subscription} from 'rxjs';
 import {ChronoService} from '../../../services/chrono/chrono.service';
+import { ApiService } from '@app/services/api/api.service';
+import { ECOE } from '@app/models';
+//import { Organization } from '@app/models';
 
+interface Organization {
+  $uri: any;
+  name: string;
+}
 @Component({
   selector: 'app-outside',
   templateUrl: './outside.component.html',
@@ -14,10 +21,14 @@ export class OutsideComponent implements OnInit, OnDestroy {
   ecoesConfig: ECOEConfig[] = [];
   selectedRound: InfoData;
   selectedConfig: ECOEConfig;
+  organizationsList: Organization[] = [];
+  ecoesList: ECOE[] = [];
 
   chronoSubs: Subscription;
 
-  constructor(private chronoService: ChronoService) {}
+  constructor(private chronoService: ChronoService,
+              private api: ApiService
+  ) {}
 
   ngOnInit() {
     this.chronoSubs = this.chronoService.getChronoConfiguration()
@@ -25,9 +36,36 @@ export class OutsideComponent implements OnInit, OnDestroy {
       (result: ECOEConfig[]) => {
         if (result && result.length > 0) {
           this.ecoesConfig = result;
+          console.log("Config 1", this.ecoesConfig);
           this.onChangeRound(this.ecoesConfig[0].rounds[0]);
         }
       }, error => {
+        console.warn(error);
+      }
+    );
+    this.getOrganizations();
+    this.getEcoes();
+  }
+
+  getOrganizations() {
+    this.api.getResource("organizations").subscribe(
+      (response: any) => {
+        this.organizationsList = Object.keys(response).map(key => response[key]);
+        console.log("Organizations", this.organizationsList);
+      },
+      (error) => {
+        console.warn(error);
+      }
+    );
+  }
+
+  getEcoes() {
+    this.api.getResource("ecoes").subscribe(
+      (response: any) => {
+        this.ecoesList = Object.keys(response).map(key => response[key]);
+        console.log("Ecoes", this.ecoesList);
+      },
+      (error) => {
         console.warn(error);
       }
     );
