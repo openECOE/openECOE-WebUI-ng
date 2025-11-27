221461faf4f03c0fcdcb0aeb73d18445f796cf0b
Alex <80858832+Humanoidear@users.noreply.github.com>
2025-11-27 20:58:23 +0100

    Fix port mismatch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   production: false,
   TESTING: false,
   DEBUG: true,
-  API_ROUTE: "http://localhost:5000/backend",
-  CHRONO_ROUTE: "http://localhost:5001",
-  BACK_ROUTE: "http://localhost:5000",
+  API_ROUTE: "http://localhost:8081/backend",
+  CHRONO_ROUTE: "http://localhost:8081/chrono",
+  BACK_ROUTE: "http://localhost:8081",
 
   //   private readonly URI_SOCKETIO = environment.CHRONO_ROUTE;
 };
