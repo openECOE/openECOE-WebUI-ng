a975f0ada8ebf7f7b7b69ca6e1ea727636778950
Alberto Moreno <amoreno@umh.es>
2026-05-22 13:16:32 +0200

    FIX: Update login form to use empty fields and change console log to debug for CSV parse results

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -25,8 +25,8 @@ export class LoginComponent implements OnInit {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 
     this.validateForm = this.formBuilder.group({
-      email: ['ecoe@umh.es', [Validators.required]],
-      password: ['Kui0chee', [Validators.required]]
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
